1e05c94304abf51b3dc2ea6dc3fa229d26ac9281
Marco_Girela <marcogirelavida@gmail.com>
2023-09-24 17:16:14 +0200

    a

---
+++ irrgarten/src/Irrgarten.java
@@ -0,0 +1,17 @@
+
+package irrgarten_pdoo;
+
+/**
+ *
+ * @author marco
+ */
+public class Irrgarten {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
