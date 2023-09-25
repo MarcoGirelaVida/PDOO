b2d967f08f414e1958338b78fe7fbedced6deae0
Marco_Girela <marcogirelavida@gmail.com>
2023-09-25 13:33:08 +0200

    a

--- irrgarten_ruby/weapon.rb
+++ irrgarten_ruby/weapon.rb
@@ -1,4 +1,39 @@
 # frozen_string_literal: true
 
 class Weapon
+  @protection
+  @uses
+
+Shield(protection, uses)
+  if protection < 0 || uses < 0
+    throw new IllegalArgumentException("Parámetros incorrectos.")
+  end
+
+  this.protection = protection
+  this.uses = uses
+  end
+
+  Shield
+    this(0.0f, 0)
+  end
+
+  protect
+
+    effectiveProtection = 0
+
+    if (uses > 0){
+      effectiveProtection = protection
+    uses--
+    end
+
+    return effectiveProtection
+  end
+
+  toString
+    return "S[" + protection + ", " + uses + "]"
+  end
+
+  discard
+    return Dice.discardElement(uses)
+  end
 end
